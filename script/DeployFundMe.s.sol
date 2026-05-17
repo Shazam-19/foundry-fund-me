babc382ca386dead5303e3b88b17639dae8754e9
shazam <abdelrahman.sayed2015.p6636@gmail.com>
2026-05-17 19:03:01 +0300

    Added HelperConfig File

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -6,17 +6,28 @@ import {Script} from "forge-std/Script.sol";
 // Import the FundMe contract so it can be deployed in this script
 import {FundMe} from "../src/FundMe.sol";
 
+import {HelperConfig} from "./HelperConfig.s.sol";
+
 // This contract handles the deployment of the FundMe contract
 contract DeployFundMe is Script {
     // The `run` function is automatically called when executing this script
     function run() external returns (FundMe) {
+
+        // Anything before startBroadcast -> Not a "real" tx
+        HelperConfig helperConfig = new HelperConfig();
+
+        // Since we are returning a struct, if it has multiple keys, then we must wrap it into ()
+        // But since we only have one, we can omit the ()
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         // Begin broadcasting transactions to the blockchain
         // All contract creation or state-changing operations between startBroadcast()
         // and stopBroadcast() are sent to the network
+        // Anything after startBroadcast -> Real tx!
         vm.startBroadcast();
 
         // Deploy a new instance of the FundMe contract
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
 
         // Stop broadcasting transactions
         // After this point, any further contract interactions are not sent to the blockchain
---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+// 1. Deploy mocks when we are on a local anvil chain
+// 2. Keep track of contract address across different chains
+
+// Import Script from Forge standard library to enable deployment scripting
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        }
+        else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feeed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feeed address
+    }
+}
