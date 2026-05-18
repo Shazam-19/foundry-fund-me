58ddcc4ce7bf491071bd1f9deb166d7cdd24aa62
shazam <abdelrahman.sayed2015.p6636@gmail.com>
2026-05-18 03:56:20 +0300

    Added Mock File

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -9,7 +9,10 @@ pragma solidity ^0.8.26;
 // Import Script from Forge standard library to enable deployment scripting
 import {Script} from "forge-std/Script.sol";
 
-contract HelperConfig {
+// Import the mock Chainlink price feed contract used for local testing
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
     // If we are on a local anvil, we deploy mocks
     // Otherwise, grab the existing address from the live network
 
@@ -40,7 +43,9 @@ contract HelperConfig {
     // Returns the ETH/USD price feed configuration for Sepolia
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         // Chainlink ETH/USD price feed address on Sepolia
-        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+            });
 
         return sepoliaConfig;
     }
@@ -48,14 +53,34 @@ contract HelperConfig {
     // Returns the ETH/USD price feed configuration for Ethereum Mainnet
     function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
         // Chainlink ETH/USD price feed address on Mainnet
-        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+            });
 
         return ethConfig;
     }
 
-    // Returns the local Anvil configuration
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
-        // Placeholder for local mock price feed address
-        // A mock contract address would normally be returned here
+    // Returns the configuration for the local Anvil network
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Deploy a mock price feed contract locally
+        // This simulates the behavior of a real Chainlink price feed
+
+        vm.startBroadcast();
+
+        // Create a mock ETH/USD price feed
+        // Parameters:
+        // 8      -> Number of decimals used by the ETH price feed
+        // 2000e8 -> Initial ETH price = 2000.00000000
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+
+        vm.stopBroadcast();
+
+        // Store the mock price feed address inside the network configuration struct
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed:address(mockPriceFeed)
+        });
+
+        // Return the local Anvil configuration
+        return anvilConfig;
     }
 }
